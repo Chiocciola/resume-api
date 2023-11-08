df1ea79d90c094c4999ca9e1d078bdbb418c6f34
Dmitriy Bondar <di-man@yandex.ru>
2023-11-07 16:58:03 -0800

    Updated /resume/sections endpoint to return

--- api/app/resume/sections/route.ts
+++ api/app/resume/sections/route.ts
@@ -1,9 +1,12 @@
 export async function GET() {
 	
+	const host = "https://api.dmitriy-bondar.dev";
+
 	const data = [
-			{url:"/api/resume/sections/skills"}, 
-			{url:"/api/resume/sections/experience"},
-			{url:"/api/resume/sections/education"}
+			{url: host + "/resume/sections/skills"}, 
+			{url: host + "/resume/sections/experience"},
+			{url: host + "/resume/sections/education"},
+			{url: host + "/resume/sections/languages"},
 		];
 
 	return Response.json(data)
