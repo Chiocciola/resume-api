74e760c0770dbca412ce0e3c111de1a314d3f4b9
Dmitriy Bondar <di-man@yandex.ru>
2023-11-07 23:21:16 -0800

    Remove delay from API

--- api/app/resume/route.ts
+++ api/app/resume/route.ts
@@ -10,7 +10,5 @@ export async function GET() {
 		summary: "Technical leader with over 15 years of experience specialized in .NET and User Interface design. Skilled in executing large scale projects at various levels of maturity. Proficient throughout all phases of software development — from concept to delivery and maintenance. Proven ability to bridge business and technical requirements in product and feature launches.",
 	};
 
-	await new Promise(r => setTimeout(r, 10000));
-
 	return Response.json(data)
   }
