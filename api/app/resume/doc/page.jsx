755c7da679f803e0cc2623440d5aa94c39c42f19
Dmitriy Bondar <di-man@yandex.ru>
2023-11-11 13:37:39 -0800

    Dispplay API entry point on API doc page

--- api/app/resume/doc/page.jsx
+++ api/app/resume/doc/page.jsx
@@ -1,8 +1,12 @@
 "use client"
 
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
+
 
 const ResumePage = () => {
+
+    const [apiEntryPoint, setApiEntryPoint] = useState("/resume");
+
     const [indexData, setIndexData] = useState(null);
     const [generalData, setGeneralData] = useState(null);
     const [sectionsData, setSectionsData] = useState(null);
@@ -23,42 +27,46 @@ const ResumePage = () => {
         setData(null);
     };
 
+    useEffect(() => {
+        setApiEntryPoint(window.location.origin + '/resume');
+    }, []);
+
     const endpoints = [
         {
             name: "index",
             description: 'list of available endpoints',
-            endPoint: '/resume',
+            endpoint: '/',
             data: indexData,
             setData: setIndexData,
         },
         {
             name: "general",
             description: 'general resume information such as name, title, and contact details.',
-            endPoint: '/resume/general',
+            endpoint: '/general',
             data: generalData,
             setData: setGeneralData,
         },
         {
             name: 'sections',
-            endPoint: '/resume/sections',
+            endpoint: '/sections',
             data: sectionsData,
             setData: setSectionsData,
         },
         {
             name: 'experience',
-            endPoint: '/resume/sections/experience',
+            endpoint: '/sections/experience',
             data: experienceData,
             setData: setExperienceData,
         },
         {
             name: 'education',
-            endPoint: '/resume/sections/education',
+            endpoint: '/sections/education',
             data: educationData,
             setData: setEducationData,
         },
         {
             name: 'skills',
-            endPoint: '/resume/sections/skills',
+            endpoint: '/sections/skills',
             data: skillsData,
             setData: setSkillsData,
         },
@@ -69,18 +77,21 @@ const ResumePage = () => {
             <h1>Resume API</h1>
             <p>The Resume API allows developers to access resume content, providing detailed information about an individual&apos;s professional and educational background. This API offers endpoints for retrieving general information, such as name and contact details, as well as detailed sections for experience and education.</p>
 
+            <h2>Entry Point</h2>
+            <p><a href={apiEntryPoint}>{apiEntryPoint}</a></p>
+
             <h2>Endpoints</h2>
 
-                {endpoints.map(({ name, description, endPoint, data, setData }) => (
+                {endpoints.map(({ name, description, endpoint, data, setData }) => (
                     <>
                         <p key={name}><strong>{name}: </strong>{description}</p>
 
-                        <table key={endPoint} style={{borderCollapse: "collapse", border: "1px solid #082044", width: "100%"}}>
+                        <table key={endpoint} style={{borderCollapse: "collapse", border: "1px solid #082044", width: "100%"}}>
                             <tbody>
                                 <tr style={{color: "white", backgroundColor: "#082044",}}>
-                                    <td><p style={{margin: "0.5rem 1rem"}}>{endPoint}</p></td>
+                                    <td><p style={{margin: "0.5rem 1rem"}}>{endpoint}</p></td>
                                     <td style={{width: "90px"}}>
-                                        {!data && (<button style={{width: "80px", border: "none", color: "white", backgroundColor: "orangered"}} onClick={() => fetchData(endPoint, setData)}>{loading ? "Loading..." : "GET"}</button>)}
+                                        {!data && (<button style={{width: "80px", border: "none", color: "white", backgroundColor: "orangered"}} onClick={() => fetchData(apiMointPoint + endpoint, setData)}>{loading ? "Loading..." : "GET"}</button>)}
                                         { data && (<button style={{width: "80px", border: "none", color: "white", backgroundColor: "orangered"}} onClick={() => clearData(setData)}>CLEAR</button>)}
                                     </td>                                             
                                 </tr>
