d1261c79d054ea9787602061f528120d0f49cd55
Dmitriy Bondar <di-man@yandex.ru>
2023-11-12 22:56:39 -0800

    Hide languages section

--- api/app/resume/sections/skills/route.ts
+++ api/app/resume/sections/skills/route.ts
@@ -7,6 +7,7 @@ export async function GET() {
 		{kind: "Web technologies", skills: ["React", "Next.js", "Firebase", "REST API", "CSS", "HTML"]},
 		{kind: "Semi technologies & standards", skills: ["TwinCAT PLC", "SECS", "GEM", "SEMI standards"]},
 		{kind: "Soft skills", skills: ["Team leadership", "Technical leadership", "Mentoring", "Cross-functional collaboration"]},
+		{kind: "Spoken languages", skills: ["English", "Italian", "Russian"]},		
 	]};
 
 	return Response.json(data)
