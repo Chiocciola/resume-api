1b10c8eeea05b60bc66bc94bca264e98d7d51660
Dmitriy Bondar <di-man@yandex.ru>
2023-12-30 23:18:28 -0800

    Show loader component while resume is loading

--- frontend/components/Resume.tsx
+++ frontend/components/Resume.tsx
@@ -2,9 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import Render from './Render';
-import { Url } from './api';
+import Loader from './Loader';
+import { Url, Section } from './api';
 
-export async function getSections(endpoint: string | undefined) : Promise<any[]>
+export async function getSections(endpoint: string | undefined) : Promise<Section[]>
 {
     if (!endpoint)
     {
@@ -20,7 +21,7 @@ export async function getSections(endpoint: string | undefined) : Promise<any[]>
                 .then(r => r.ok ? r : Promise.reject(`${r.status} ${r.statusText}`))
                 .then(r => r.json());
 
-        const sectionsPromises : any[] = 
+        const sectionsPromises : Promise<Section>[] = 
             sectionUrls.map( (s : Url) => 
                 fetch(s.url)
                     .then( r => r.ok ? r : Promise.reject(`${r.status} ${r.statusText}`))
@@ -37,11 +38,16 @@ export async function getSections(endpoint: string | undefined) : Promise<any[]>
 
 export default function Resume({endpoint}: {endpoint: string | undefined}) : JSX.Element
 {   
-    const [sections, setSections] = useState([] as any[]);
+    const [sections, setSections] = useState([] as Section[]);
 
     useEffect(
         () => {getSections(endpoint).then(setSections)},
         []);
+
+    if (sections.length === 0)
+    {
+        return <div className='flex justify-center w-full text-current'><Loader show={true} color="text-current"/></div>;
+    }
 
     return <>{sections.map(Render)}</>;
 };
