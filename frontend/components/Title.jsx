cba06f2470b3c1f13c806fd3cf5206a273f679a5
Dmitriy Bondar <di-man@yandex.ru>
2024-01-02 21:17:54 -0800

    Style title with gradient

--- frontend/components/Title.jsx
+++ frontend/components/Title.jsx
@@ -11,7 +11,9 @@ export default function Title({children, className}) {
 
     const pathname = usePathname();
 
+    const gradientTailwindStyle = "bg-gradient-to-r from-amber-500 via-red-600 to-fuchsia-500 bg-clip-text text-transparent";
+
     return pathname == '/' 
-            ? <p             className={majorMonoDisplay.className + " text-2xl text-gray-500 my-auto select-none cursor-default " + className}>{children}</p>
-            : <Link href='/' className={majorMonoDisplay.className + " text-2xl text-gray-500 my-auto select-none " + className}>{children}</Link>;
+            ? <p             className={majorMonoDisplay.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none cursor-default " + className}>{children}</p>
+            : <Link href='/' className={majorMonoDisplay.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none " + className}>{children}</Link>;
 }
