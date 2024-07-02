bef66b13ae7c4ad4e67b6bf6ec91414df8b6372e
Dmitriy Bondar <di-man@yandex.ru>
2024-07-01 21:40:52 -0700

    Update title font

--- frontend/components/Title.jsx
+++ frontend/components/Title.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 import {usePathname} from 'next/navigation';
 
 
-import { Major_Mono_Display } from 'next/font/google'
-const majorMonoDisplay = Major_Mono_Display({ subsets: ['latin'], weight: '400' })
+import { Outfit } from 'next/font/google'
+const outfit = Outfit({ subsets: ['latin'], weight: '400' })
 
 export default function Title({children, className}) {
 
@@ -14,6 +14,6 @@ export default function Title({children, className}) {
     const gradientTailwindStyle = "bg-gradient-to-r from-amber-500 via-red-600 to-fuchsia-500 bg-clip-text text-transparent";
 
     return pathname == '/' 
-            ? <p             className={majorMonoDisplay.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none cursor-default " + className}>{children}</p>
-            : <Link href='/' className={majorMonoDisplay.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none " + className}>{children}</Link>;
+            ? <p             className={outfit.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none cursor-default " + className}>{children}</p>
+            : <Link href='/' className={outfit.className + " " + gradientTailwindStyle + " text-2xl text-gray-500 my-auto select-none " + className}>{children}</Link>;
 }
