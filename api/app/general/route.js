0036cdc6525563c8777e9cd61a4efaed2a3fe0dd
Dmitriy Bondar <di-man@yandex.ru>
2024-07-01 21:43:15 -0700

    Update summary

--- api/app/general/route.js
+++ api/app/general/route.js
@@ -9,7 +9,12 @@ export async function GET() {
 			// phone: "408-306-2596",
 			// email: "di-man@yandex.ru",
 			linkedin: {url: "https://www.linkedin.com/in/dmitriy-bondar/"},
-			summary: "Technical leader with over 15 years of experience specialized in .NET and User Interface design. Skilled in executing large scale projects at various levels of maturity. Proficient throughout all phases of software development — from concept to delivery and maintenance. Proven ability to bridge business and technical requirements in product and feature launches.",
+			summary: `Experienced technical leader with over 15 years of experience, specialized in .NET and Front-end
+			development. Known for successfully leading large-scale projects from inception to completion and
+			managing cross-functional teams across the globe. Skilled in UI/UX redesigns, improving code quality,
+			and transitioning technologies seamlessly. Proficient in C#, C++, TypeScript, JavaScript, React, Next.js,
+			and ASP.NET. Proven ability to align technical solutions with business needs, driving successful product
+			launches and improving team performance.`,
 		}
 	};  
 
