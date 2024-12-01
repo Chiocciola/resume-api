0c13003bf15b0df0e76f8be91313ca8cea224fd9
Dmitriy Bondar <di-man@yandex.ru>
2024-11-30 18:38:16 -0800

    Update Tailwind pt. 3

--- frontend/components/Title.jsx
+++ frontend/components/Title.jsx
@@ -11,7 +11,7 @@ export default function Title({children, className}) {
 
     const pathname = usePathname();
 
-    const gradient = "bg-gradient-to-r from-amber-500 via-red-600 to-fuchsia-500 bg-clip-text text-transparent";
+    const gradient = "bg-linear-to-r from-amber-500 via-red-600 to-fuchsia-500 bg-clip-text text-transparent";
 
     return pathname == '/' 
             ? <p             className={outfit.className + " " + gradient + " text-2xl my-auto select-none cursor-default " + className}>{children}</p>
