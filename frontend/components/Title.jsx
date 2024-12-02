72e3770a2e30d3c50f1796932abb952d1add21ce
Dmitriy Bondar <di-man@yandex.ru>
2024-12-01 22:31:44 -0800

    Animate title on hover

--- frontend/components/Title.jsx
+++ frontend/components/Title.jsx
@@ -11,9 +11,11 @@ export default function Title({children, className}) {
 
     const pathname = usePathname();
 
-    const gradient = "bg-linear-to-r from-amber-500 via-red-600 to-fuchsia-500 bg-clip-text text-transparent";
+    // bg-linear-to-r from-amber-500 via-red-600 to-fuchsia-500
+    const gradient = ` bg-clip-text text-transparent bg-[length:200%_auto]
+    bg-[linear-gradient(to_right,theme(colors.amber.500),theme(colors.red.600),theme(colors.fuchsia.500),theme(colors.red.600),theme(colors.amber.500))]`;
 
     return pathname == '/' 
             ? <p             className={outfit.className + " " + gradient + " text-2xl my-auto select-none cursor-default " + className}>{children}</p>
-            : <Link href='/' className={outfit.className + " " + gradient + " text-2xl my-auto select-none " + className}>{children}</Link>;
+            : <Link href='/' className={outfit.className + " " + gradient + " text-2xl my-auto select-none hover:animate-gradient " + className}>{children}</Link>;
 }
