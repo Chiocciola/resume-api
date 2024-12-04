b82dccfda9580179725229ba5d598805a8aeaf9b
Dmitriy Bondar <di-man@yandex.ru>
2024-12-03 22:19:41 -0800

    Introduce glow effect

--- frontend/components/Title.jsx
+++ frontend/components/Title.jsx
@@ -3,6 +3,8 @@
 import Link from 'next/link'
 import {usePathname} from 'next/navigation';
 
+import "./Title.css";
+
 
 import { Outfit } from 'next/font/google'
 const outfit = Outfit({ subsets: ['latin'], weight: '400' })
@@ -13,9 +15,22 @@ export default function Title({children, className}) {
 
     // bg-linear-to-r from-amber-500 via-red-600 to-fuchsia-500
     const gradient = ` bg-clip-text text-transparent bg-[length:200%_auto]
-    bg-[linear-gradient(to_right,theme(colors.amber.500),theme(colors.red.600),theme(colors.fuchsia.500),theme(colors.red.600),theme(colors.amber.500))]`;
+    bg-[linear-gradient(to_right,theme(colors.amber.500),theme(colors.red.600),theme(colors.fuchsia.500),theme(colors.red.600),theme(colors.amber.500))] `;
 
-    return pathname == '/' 
-            ? <p             className={outfit.className + " " + gradient + " text-2xl my-auto select-none cursor-default " + className}>{children}</p>
-            : <Link href='/' className={outfit.className + " " + gradient + " text-2xl my-auto select-none hover:animate-gradient " + className}>{children}</Link>;
+    const style = outfit.className + " " + gradient + " text-2xl my-auto select-none ";
+
+    return  (
+        <div className='relative m-auto glowingText'>
+
+            {pathname == '/' 
+            ? <p             className={style + " cursor-default "+ className}>{children}</p>
+            :            
+            <>
+              <p aria-hidden="true" className={style + " glowPart absolute -z-10"}>{children}</p> 
+              <Link href='/' className={style + " textPart " + className}>{children}</Link>
+            </>
+            }
+
+      </div>  
+    );
 }
